82d8d6ad2c114fd4323e3a00d87bbb4dd602ab54
Antonio Villagra De La Cruz <5470315+AntonioVdlC@users.noreply.github.com>
2021-08-27 23:41:39 +0200

    Implement basic range operations

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,98 @@
-function add(a: number, b: number): number {
-  return a + b;
+class Range {
+  // TODO: add forEach, map, filter ... ?
+  // TODO: add toArray, toString, ... ?
+  // TODO: allow for inclusive ranges (similar to Rust)?
+
+  _start: number;
+  _stop: number;
+  _step: number;
+
+  // Counter used for iteration, so that we can iterate multiple times over
+  // the same range
+  i: number;
+
+  constructor(start: number, stop: number, step: number = 1) {
+    if (start > stop) {
+      // TODO: allow reverse ranges?
+      throw new Error(`Cannot create a range from ${start} to ${stop}`);
+    }
+
+    if (isNaN(Number(start))) {
+      throw new Error(`${start} is not a Number`);
+    }
+    if (isNaN(Number(stop))) {
+      throw new Error(`${stop} is not a Number`);
+    }
+    if (isNaN(Number(step))) {
+      throw new Error(`${step} is not a Number`);
+    }
+
+    this._start = Number(start);
+    this._stop = Number(stop);
+    this._step = Number(step);
+
+    // Initialise a counter for iteration
+    this.i = Number(start);
+  }
+
+  /**
+   * Return the first value of a range
+   * @returns First value of a range
+   */
+  first() {
+    return this._start;
+  }
+
+  /**
+   * Return the last value of a range
+   * @returns Last value of a range
+   */
+  last() {
+    return this._stop;
+  }
+
+  /**
+   * Return the value of the step for the range
+   * @returns Value of the step
+   */
+  step() {
+    return this._step;
+  }
+
+  /**
+   * Implement iterator protocol
+   */
+  next() {
+    if (this.i < this._stop) {
+      const value = this.i;
+      this.i += this._step;
+      return { value, done: false };
+    }
+
+    // We reset the value once we have iterated over all values so that
+    // ranges are reusable.
+    this.i = this._start;
+
+    return { value: undefined, done: true };
+  }
+
+  /**
+   * Implement iterable protocol
+   */
+  [Symbol.iterator]() {
+    return this;
+  }
 }
 
-export default add;
+/**
+ * Function exported to allow for the following API: `range(start, stop, step)`
+ * @param start The value at which the range starts (inclusive)
+ * @param stop The value at which the range stops (inclusive)
+ * @param step The steps between each iteration within the range
+ * @returns Range object
+ */
+function range(start: number, stop: number, step: number = 1): Range {
+  return new Range(start, stop, step);
+}
+
+export default range;
--- test/index.test.js
+++ test/index.test.js
@@ -1,14 +1,197 @@
-import add from "../src/index.ts";
+import range from "../src/index.ts";
 
-describe("add", () => {
+describe("range", () => {
   it("is a function", () => {
-    expect(typeof add).toBe("function");
+    expect(typeof range).toBe("function");
   });
 
-  it("adds 2 number", () => {
-    const expected = 4;
-    const actual = add(2, 2);
+  it("creates a new range object", () => {
+    expect(typeof range(0, 10)).toBe("object");
+  });
 
-    expect(actual).toEqual(expected);
+  it("throws if not passed a valid range (start > stop)", () => {
+    expect(() => range(10, 0)).toThrow();
+  });
+
+  it("throws if not passed a valid range (start is not a number)", () => {
+    expect(() => range("hello", 3)).toThrow();
+  });
+
+  it("throws if not passed a valid range (stop is not a number)", () => {
+    expect(() => range(3, "hello")).toThrow();
+  });
+
+  it("throws if not passed a valid range (step is not a number)", () => {
+    expect(() => range(0, 3, "hello")).toThrow();
+  });
+
+  describe(".first()", () => {
+    it("is a function", () => {
+      expect(typeof range(0, 10).first).toBe("function");
+    });
+
+    it("returns the start value of a range", () => {
+      const start = 0;
+      const stop = 10;
+
+      const expected = start;
+      const actual = range(start, stop).first();
+
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  describe(".last()", () => {
+    it("is a function", () => {
+      expect(typeof range(0, 10).last).toBe("function");
+    });
+
+    it("returns the stop value of a range", () => {
+      const start = 0;
+      const stop = 10;
+
+      const expected = stop;
+      const actual = range(start, stop).last();
+
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  describe(".step()", () => {
+    it("is a function", () => {
+      expect(typeof range(0, 10).step).toBe("function");
+    });
+
+    it("returns the step value of a range", () => {
+      const start = 0;
+      const stop = 10;
+      const step = 1;
+
+      const expected = step;
+      const actual = range(start, stop, step).step();
+
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  describe(".next()", () => {
+    it("is a function", () => {
+      expect(typeof range(0, 10).next).toBe("function");
+    });
+
+    it("iterates over a range", () => {
+      const start = 0;
+      const stop = 5;
+
+      const r = range(start, stop);
+
+      expect(r.next()).toEqual({ done: false, value: start });
+      expect(r.next()).toEqual({ done: false, value: start + 1 });
+      expect(r.next()).toEqual({ done: false, value: start + 2 });
+      expect(r.next()).toEqual({ done: false, value: start + 3 });
+      expect(r.next()).toEqual({ done: false, value: start + 4 });
+      expect(r.next()).toEqual({ done: true, value: undefined });
+    });
+
+    it("iterates over a range (step = 2)", () => {
+      const start = 0;
+      const stop = 9;
+      const step = 2;
+
+      const r = range(start, stop, step);
+
+      expect(r.next()).toEqual({ done: false, value: start });
+      expect(r.next()).toEqual({ done: false, value: start + 2 });
+      expect(r.next()).toEqual({ done: false, value: start + 4 });
+      expect(r.next()).toEqual({ done: false, value: start + 6 });
+      expect(r.next()).toEqual({ done: false, value: start + 8 });
+      expect(r.next()).toEqual({ done: true, value: undefined });
+    });
+
+    it("iterates over a range multiple times", () => {
+      const start = 0;
+      const stop = 5;
+      const step = 1;
+
+      const r = range(start, stop, step);
+
+      expect(r.next()).toEqual({ done: false, value: start });
+      expect(r.next()).toEqual({ done: false, value: start + 1 });
+      expect(r.next()).toEqual({ done: false, value: start + 2 });
+      expect(r.next()).toEqual({ done: false, value: start + 3 });
+      expect(r.next()).toEqual({ done: false, value: start + 4 });
+      expect(r.next()).toEqual({ done: true, value: undefined });
+      expect(r.next()).toEqual({ done: false, value: start });
+      expect(r.next()).toEqual({ done: false, value: start + 1 });
+      expect(r.next()).toEqual({ done: false, value: start + 2 });
+      expect(r.next()).toEqual({ done: false, value: start + 3 });
+      expect(r.next()).toEqual({ done: false, value: start + 4 });
+      expect(r.next()).toEqual({ done: true, value: undefined });
+      expect(r.next()).toEqual({ done: false, value: start });
+      expect(r.next()).toEqual({ done: false, value: start + 1 });
+      expect(r.next()).toEqual({ done: false, value: start + 2 });
+      expect(r.next()).toEqual({ done: false, value: start + 3 });
+      expect(r.next()).toEqual({ done: false, value: start + 4 });
+      expect(r.next()).toEqual({ done: true, value: undefined });
+    });
+  });
+
+  describe("for..of", () => {
+    it("is an iterable", () => {
+      expect(typeof range(0, 10)[Symbol.iterator]).toBe("function");
+    });
+
+    it("iterates over a range", () => {
+      const start = 0;
+      const stop = 5;
+
+      const expected = [0, 1, 2, 3, 4];
+
+      const actual = [];
+      for (let i of range(start, stop)) {
+        actual.push(i);
+      }
+
+      expect(actual).toEqual(expected);
+    });
+
+    it("iterates over a range (step = 2)", () => {
+      const start = 0;
+      const stop = 9;
+      const step = 2;
+
+      const expected = [0, 2, 4, 6, 8];
+
+      const actual = [];
+      for (let i of range(start, stop, step)) {
+        actual.push(i);
+      }
+
+      expect(actual).toEqual(expected);
+    });
+
+    it("iterates over a range multiple times", () => {
+      const start = 0;
+      const stop = 9;
+      const step = 2;
+
+      const r = range(start, stop, step);
+
+      const expected = [0, 2, 4, 6, 8];
+
+      const actual = [];
+      for (let i of r) {
+        actual.push(i);
+      }
+
+      expect(actual).toEqual(expected);
+
+      const actual2 = [];
+      for (let i of r) {
+        actual2.push(i);
+      }
+
+      expect(actual2).toEqual(expected);
+    });
   });
 });
